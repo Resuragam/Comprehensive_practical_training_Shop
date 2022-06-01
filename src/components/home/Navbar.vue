05c9f4bb4c8960ca7529bea8cc780883cf3a2701
Resurgam <1791614500@qq.com>
2022-06-01 09:47:38 +0800

    navbar

--- src/components/home/Navbar.vue
+++ src/components/home/Navbar.vue
@@ -2,22 +2,33 @@
 import { ref } from "vue";
 import { Toast } from "vant";
 const value = ref("");
-const onSearch = (val:any) => Toast(val);
+const active = ref(0);
+const onSearch = (val: any) => Toast(val);
 const onClickButton = () => Toast(value.value);
 </script>
 
 <template>
   <div>
-    <van-search
-      v-model="value"
-      show-action
-      label="地址"
-      placeholder="请输入搜索关键词"
-      @search="onSearch"
-    >
-      <template #action>
-        <div @click="onClickButton">搜索</div>
-      </template>
-    </van-search>
+    <div>
+      <van-search
+        v-model="value"
+        show-action
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+      >
+        <template #action>
+          <div class="h-34px">
+            <div class="text-center font-mono font-600 leading-5">ALL</div>
+            <div class="text-xs leading-3">分类</div>
+          </div>
+        </template>
+      </van-search>
+    </div>
+    <div>
+      <van-tabs v-model:active="active" color="#000000">
+        <van-tab v-for="index in 8" :title="'标签 ' + index">
+        </van-tab>
+      </van-tabs>
+    </div>
   </div>
 </template>
--- src/view/home/Home.vue
+++ src/view/home/Home.vue
@@ -1,13 +1,14 @@
 <script lang='ts' setup name="home">
 import { useRoute } from "vue-router";
 import { onMounted, ref } from "vue";
+import navbar from "@/components/home/Navbar.vue";
 const route = useRoute();
 
-// 控制navbr
-const navbarActive = ref(0);
+
 console.log(route.path);
 </script>
 
 <template>
   <navbar></navbar>
+  <router-view></router-view>
 </template>
